a839888dd15642b69e51e1c5171ca48350e7f44f
Nandini Jindal <nandinijindal010@gmail.com>
2024-07-13 10:12:39 +0530

    images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['images.unsplash.com','res.cloudinary.com']
-    }
+    images: {
+        domains: ['images.unsplash.com', 'res.cloudinary.com'],
+        unoptimized: true, // Disable Image Optimization
+    },
+    output: "export", 
 };
 
 export default nextConfig;
